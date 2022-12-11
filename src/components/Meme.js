83c0c5a735562559301cde469072043b4f73aa28
akshaykh10 <akshaykhemanth@gmail.com>
2022-12-11 22:37:38 +0530

    grid layout

--- src/components/Meme.js
+++ src/components/Meme.js
@@ -1,10 +1,17 @@
-
+import '../index.css'
 export default function Meme(){
+
+    function logConsole(){
+        console.log("Clicked")
+    }
     return (
-        <div>
-            <input></input>
-            <input></input>
-            <button></button>
-        </div>
+       
+            <div className='form' >
+                <input type="text" className="meme-input"/>
+                <input type="text" className="meme-input"/>
+                <button  onClick={logConsole}  className='form-button'>Get a new meme image</button>
+            </div>
+            
+        
     )
 }
